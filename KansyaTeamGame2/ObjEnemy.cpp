8437040dae09b9060246571429706fa259f4c53b
ShindoMahito <72609092+ShindoMahito@users.noreply.github.com>
2020-10-22 23:38:26 +0900

    敵　移動　当たり判定　作成　画像変えてない 　　マナの情報収得すれば後は楽

--- KansyaTeamGame2/ObjEnemy.cpp
+++ KansyaTeamGame2/ObjEnemy.cpp
@@ -1,6 +1,8 @@
 //使用するヘッダーファイル
 #include "GameL/DrawTexture.h"
 #include "GameHead.h"
+#include "GameL\HitBoxManager.h"
+
 #include "ObjEnemy.h"
 
 //使用するネームベース
@@ -9,13 +11,29 @@ using namespace GameL;
 //イニシャライズ
 void CObjEnemy::Init()
 {
-
+	m_ex = 0.0f;
+	m_ey = 400.0f;
+	m_vx = 0.5f;
+	m_vy = 0.0f;
+	//当たり判定用のHITBOXを作成
+	Hits::SetHitBox(this, m_ex, m_ey, 50, 50, ELEMENT_ENEMY, OBJ_ENEMY, 10);
 }
 
 //アクション
 void CObjEnemy::Action()
 {
+	//m_vxの速度で移動
+	m_ex += m_vx;
 
+	//HitBOxの内容を変更
+	CHitBox* hit = Hits::GetHitBox(this);
+	hit->SetPos(m_ex + 10.0f, m_ey + 10.0f);
+	
+	//特定の位置で停止（マナの情報を収得してやりたい）
+	if (m_ex == 268)
+	{
+		m_vx = 0.0f;
+	}
 }
 
 //ドロー
@@ -28,15 +46,15 @@ void CObjEnemy::Draw()
 	RECT_F dst;//描画先表示位置
 
 	//切り取り位置の設定
-	src.m_top = 0.0f;
-	src.m_left = 0.0f;
-	src.m_right = 50.0f;
-	src.m_bottom = 50.0f;
+	src.m_top    = 0.0f;
+	src.m_left   = 0.0f;
+	src.m_right  = 64.0f;
+	src.m_bottom = 64.0f;
 	//表示位置の設定
-	dst.m_top = 0.0f;
-	dst.m_left = 50.0f;
-	dst.m_right = 100.0f;
-	dst.m_bottom = 50.0f;
+	dst.m_top    = m_ey;
+	dst.m_left	 = m_ex + 64.0f;
+	dst.m_right  = m_ex;
+	dst.m_bottom = m_ey + 64.0f;
 
 	//描画
 	Draw::Draw(0, &src, &dst, c, 0.0f);
